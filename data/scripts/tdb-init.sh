4771a0cecea93d4e5369347e79e1bc52790a3c71
Adrian Gschwend <adrian.gsschwend@zazuko.com>
2015-02-05 19:09:40 +0100

    Erste SPARQL queries für den Frontend, TDB Script

---
+++ data/scripts/tdb-init.sh
@@ -0,0 +1,5 @@
+tdbloader --graph=http://data.alod.ch/graph/bs --loc=DB ~/workspace/zazuko.alod/data/graphs/bs_all.nt
+tdbloader --graph=http://data.alod.ch/graph/ne --loc=DB ~/workspace/zazuko.alod/data/graphs/ne_2014_12_21.ttl
+tdbloader --graph=http://data.alod.ch/graph/ge --loc=DB ~/workspace/zazuko.alod/data/graphs/ge_all.nt
+tdbloader --graph=http://data.alod.ch/graph/bar --loc=DB ~/workspace/zazuko.alod/data/graphs/bar_2015_01_06.ttl
+tdbloader --graph=http://data.alod.ch/graph/skos --loc=DB ~/workspace/zazuko.alod/data/graphs/tectonics.ttl
