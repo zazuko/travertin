fca4d33c8fd2b32ece174686227befc7f71c770e
bergi <bergi@axolotlfarm.org>
2015-02-06 21:45:15 +0100

    Paging in die Suche eingebaut + kleine Verbesserungen

--- data/public/js/search.js
+++ data/public/js/search.js
@@ -21,70 +21,151 @@ var context = {
 var ResultTable = React.createClass({
   getInitialState: function() {
     return {
-      data: [{title: 'test'}]
+      page: 0,
+      cache: {},
+      data: []
     }
   },
-  loadData: function() {
+  search: function () {
+    var self = this;
+
+    self.setState({cache: {}, data: []});
+
+    self.setPage(0);
+  },
+  setPage: function (page) {
+    var self = this;
+
+    self.loadResults(page)
+      .then(function (results) {
+        self.setState({page: page, data: results});
+
+        self.updateCache();
+      });
+  },
+  turnPage: function (direction, event) {
+    var self = this;
+
+    if (event != null) {
+      event.preventDefault();
+    }
+
+    if (self.state.page + direction >= 0) {
+      self.setPage(self.state.page + direction);
+    }
+  },
+  updateCache: function () {
+    var self = this;
+
+    if (self.state.page > 0) {
+      self.loadResults(self.state.page-1);
+    }
+
+    self.loadResults(self.state.page+1);
+  },
+  loadResults: function(page) {
     var
       self = this,
       searchString = $('#search-string').val();
 
-    $.ajax({
-      url: '/alod/search?q=' + encodeURIComponent(searchString),
-      headers: {Accept: 'application/ld+json'},
-      success: function(data) {
-        jsonld.promises().compact(data, context)
-          .then(function (data) {
-            if ('@graph' in data) {
-              data = data['@graph'];
-            } else {
-              data = [data];
-            }
+    var doRequest = function (searchString, page) {
+      return new Promise(function (resolve, reject) {
+        $.ajax({
+          url: '/alod/search?q=' + encodeURIComponent(searchString) + '&page=' + (page + 1),
+          headers: {Accept: 'application/ld+json'},
+          success: function (data) {
+            jsonld.promises().compact(data, context)
+              .then(function (data) {
+                var cache = self.state.cache;
 
-            self.setState({data: data});
-          })
-          .catch(function (error) {
-            console.error(error.stack);
-          });
-      }
-    })
+                if ('@graph' in data) {
+                  data = data['@graph'];
+                } else {
+                  data = [data];
+                }
+
+                cache[page] = data;
+
+                self.setState({cache: cache});
+
+                resolve(data);
+              })
+          },
+          error: function () {
+            reject();
+          }
+        });
+      });
+    };
+
+    if (page in self.state.cache) {
+      return Promise.resolve(self.state.cache[page]);
+    } else {
+      return doRequest(searchString, page);
+    }
   },
   componentDidMount: function() {
     var self = this;
 
-    $('#search').on('click', self.loadData);
+    $('#search').on('click', self.search);
   },
-  componentDidUpdate: function() {
+  componentWillUnmount: function() {
     var self = this;
 
-    $('#search').off('click', self.loadData);
+    $('#search').off('click', self.search);
   },
   render: function() {
     var self = this;
 
+    var value = function (property) {
+      if (typeof property === 'string') {
+        return property;
+      } else if ('@value' in property) {
+        return property['@value'];
+      }
+    };
+
     var rows = self.state.data.map(function (row, index) {
       return React.DOM.tr({},
-        React.DOM.td({}, index + 1),
-        React.DOM.td({}, row.title));
+        React.DOM.td({}, self.state.page*self.props.resultsPerPage + index + 1),
+        React.DOM.td({},
+          React.DOM.a({href: row['@id']}, value(row.title))));
     });
 
-    return React.DOM.div({className: 'table-responsive'},
-    React.DOM.table({className: 'table table-bordered', id: 'results-table'},
+    var table = React.DOM.table({className: 'table table-bordered', id: 'results-table'},
       React.DOM.thead({},
         React.DOM.tr({},
-          React.DOM.th({}, 'row'),
-          React.DOM.th({}, 'title')
-        )
-      ),
-      React.DOM.tbody({}, rows)
-    )
-    );
+          React.DOM.th({}, 'Nr.'),
+          React.DOM.th({}, 'Titel'))),
+      React.DOM.tbody({}, rows));
+
+    var noEntries = React.DOM.p({}, 'keine Treffer');
+
+    var pageIsNotEmpty = function (page) {
+      if (!(page in self.state.cache)) {
+        return false;
+      }
+
+      // there is at least one context entry
+      return self.state.cache[page].length > 1;
+    };
+
+    var pager = React.DOM.nav({},
+      React.DOM.ul({className: 'pager'},
+        pageIsNotEmpty(self.state.page-1) ? React.DOM.li({className: 'previous'},
+          React.DOM.a({href: '#', onClick: self.turnPage.bind(self, -1)}, 'Previous')) : null,
+        pageIsNotEmpty(self.state.page+1) ? React.DOM.li({className: 'next'},
+          React.DOM.a({href: '#', onClick: self.turnPage.bind(self, +1)}, 'Next')) : null));
+
+    return React.DOM.div({},
+      self.state.data.length > 0 ? table : noEntries,
+      pager);
   }
 });
 
 var createResultTable = React.createFactory(ResultTable);
 
 
-var results = createResultTable({id: 'results'});
+var results = createResultTable({id: 'results', resultsPerPage: 10});
 
 React.render(results, document.getElementById('results'));
