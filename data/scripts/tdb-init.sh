12e4d7a46cc522639f6ed37f957d6ed7e1cf7dd8
Adrian Gschwend <adrian.gsschwend@zazuko.com>
2015-02-06 10:56:59 +0100

    Fuseki TDB script update

--- data/scripts/tdb-init.sh
+++ data/scripts/tdb-init.sh
@@ -1,5 +1,8 @@
-tdbloader --graph=http://data.alod.ch/graph/bs --loc=DB ~/workspace/zazuko.alod/data/graphs/bs_all.nt
-tdbloader --graph=http://data.alod.ch/graph/ne --loc=DB ~/workspace/zazuko.alod/data/graphs/ne_2014_12_21.ttl
-tdbloader --graph=http://data.alod.ch/graph/ge --loc=DB ~/workspace/zazuko.alod/data/graphs/ge_all.nt
-tdbloader --graph=http://data.alod.ch/graph/bar --loc=DB ~/workspace/zazuko.alod/data/graphs/bar_2015_01_06.ttl
-tdbloader --graph=http://data.alod.ch/graph/skos --loc=DB ~/workspace/zazuko.alod/data/graphs/tectonics.ttl
+#!/bin/sh
+export FUSEKI_HOME=/data/bin/jena-fuseki-1.1.1
+tdbloader --graph=http://data.alod.ch/graph/bs --loc=/data/fusekidb/alod/DB ../graphs/bs_all.nt
+tdbloader --graph=http://data.alod.ch/graph/ne --loc=/data/fusekidb/alod/DB ../graphs/ne_2014_12_21.ttl
+tdbloader --graph=http://data.alod.ch/graph/ge --loc=/data/fusekidb/alod/DB ../graphs/ge_all.nt
+tdbloader --graph=http://data.alod.ch/graph/bar --loc=/data/fusekidb/alod/DB ../graphs/bar_2015_01_06.ttl
+tdbloader --graph=http://data.alod.ch/graph/generic --loc=/data/fusekidb/alod/DB ../graphs/tectonics.ttl
+java -cp $FUSEKI_HOME/fuseki-server.jar jena.textindexer --desc=/data/etc/fuseki.ttl
