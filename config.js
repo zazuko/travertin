d5bf6a95e19d025ff9d6b95e1eee4fbdd11a4af5
bergi <bergi@axolotlfarm.org>
2015-02-08 14:58:14 +0100

    räume Fuseki Headers im Patch Headers Module auf

--- config.js
+++ config.js
@@ -17,6 +17,24 @@ var buildExistsQuery = function (iri) {
 
 var patchResponseHeaders = function (res, headers) {
   if (res.statusCode === 200) {
+    // clean existings values
+    var fieldList = [
+      'Access-Control-Allow-Origin',
+      'Cache-Control',
+      'Fuseki-Request-ID',
+      'Server',
+      'Vary'];
+
+    fieldList.forEach(function (field) {
+      if (field in res._headers) {
+        delete res._headers[field];
+      }
+
+      if (field.toLowerCase() in res._headers) {
+        delete res._headers[field.toLowerCase()];
+      }
+    });
+
     // cors header
     headers['Access-Control-Allow-Origin'] = '*';
 
