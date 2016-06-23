df909a02f2ec685298dca4f50504b6921a5732c0
Michael Luggen <michael@portos.fritz.box>
2016-06-23 23:31:49 +0200

    fixes

--- modules/alod-web-interface/build.js
+++ modules/alod-web-interface/build.js
@@ -1,7 +1,9 @@
 require('shelljs/global')
 
-exec('browserify -d lib/search-result-list.js -o dist/search-result-list.js')
-cp('node_modules/clusterize.js/clusterize.css', 'dist')
-cp('-r', 'public/*', 'dist')
+mkdir('-p','dist')
 
-cp('-r', 'dist/*', '../../public')
+exec('browserify -d lib/search-result-list.js -o dist/search-result-list.js')
+cp('node_modules/clusterize.js/clusterize.css', 'dist/')
+cp('-r', 'public/*', 'dist/')
+
+cp('-r', 'dist/*', '../../data/public')
