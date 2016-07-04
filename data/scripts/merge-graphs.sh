a5abf9d5baa91b3eb4aff8702a92ad0630758778
Adrian Gschwend <ktk@netlabs.org>
2016-07-04 16:10:56 +0200

    Cleanup

--- data/scripts/merge-graphs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cat ../graphs/ge_2015_02_08_*  | rapper -i turtle -o ntriples - http://example.org/ > ../graphs/ge_all.nt
-cat ../graphs/bs_2015_02_08_*  | rapper -i turtle -o ntriples - http://example.org/ > ../graphs/bs_all.nt
