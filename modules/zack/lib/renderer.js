0386d37f330c94692fa93eaa28aa657aa17ae24a
Michael Luggen <github.michael@oiu.ch>
2016-07-29 15:20:36 +0200

    first filterable element

--- modules/zack/lib/renderer.js
+++ modules/zack/lib/renderer.js
@@ -3,6 +3,8 @@ var colorHash = new (require('color-hash'))
 var renderer = {}
 
 renderer.renderResult = function (page, subject) {
+  var rendering = ''
+
   var level = page.match(subject, 'http://data.archiveshub.ac.uk/def/level').toArray().shift()
 
   var levelString = level.toString()
@@ -11,12 +13,22 @@ renderer.renderResult = function (page, subject) {
 
   var title = page.match(subject, 'http://purl.org/dc/elements/1.1/title').toArray().shift()
 
+  var titleString = '<a href="' + subject.toString() + '">' + title.object.toString() + '</a>'
+
   var referenceCode = page.match(subject, 'http://data.alod.ch/alod/referenceCode').toArray().shift()
   var recordId = page.match(subject, 'http://data.alod.ch/alod/recordID').toArray().shift()
+
 
   var referenceString = recordId.object.toString()
   if (referenceCode) {
     referenceString = referenceCode.object.toString()
+  }
+  var reference = '<i>' + referenceString + '</i>'
+
+  var maintenanceAgencyCode = page.match(subject, 'http://data.archiveshub.ac.uk/def/maintenanceAgencyCode').toArray().shift()
+  var agency = ''
+  if (maintenanceAgencyCode) {
+      maintenanceAgency = '<div data-predicate="'+maintenanceAgencyCode.predicate.toString()+'" class="filterable" onclick="addFilter(this)">'+maintenanceAgencyCode.object.toString()+'</div>'
   }
 
   var intervalStarts = page.match(subject, 'http://www.w3.org/2006/time#intervalStarts').toArray().shift()
@@ -30,7 +42,9 @@ renderer.renderResult = function (page, subject) {
     }
   }
 
-  return '<div class="zack-result"><div class="result-level-wrap"><div class="vertical-text result-level" style="background-color: ' + levelColor + '">' + levelShort + '</div></div>' + '<div class="result-main">' + timeTick + '<a href="' + subject.toString() + '">' + title.object.toString() + '</a></br><i>' + referenceString + '</i></div></div>'
+  rendering =  '<div class="zack-result row"><div class="one-third column"><div class="result-level-wrap"><div class="vertical-text result-level" style="background-color: ' + levelColor + '">' + levelShort + '</div></div>' + '<div class="result-main">' + timeTick + titleString + '</br>' + reference + '</div></div><div class="two-thirds column">'+maintenanceAgency+'</div></div>'
+
+   return rendering
 }
 
 module.exports = renderer
